511325298f552975145c2c9ab868d75d632e9a83
Flygonyu <vanjaschristensen@gmail.com>
2021-09-22 13:07:28 +0200

    unit testing done! wee

---
+++ controller.js
@@ -0,0 +1,48 @@
+
+    
+        // controller 
+        function choose(bar){
+            
+        if(bar == chosenBar){
+            disable = 'disabled';
+            chosenBar = '<i>none</i>'
+        }  
+        else{
+            chosenBar = bar;
+            disable = ''
+        }
+            show();
+        }
+
+        function changeBar(){
+            if (inputValue < 1 || inputValue > 10){
+            alert("Please choose a number between 1 and 10!")
+        }
+            else{
+                numbers[chosenBar-1] = inputValue;
+                chosenBar = '<i>none</i>';
+                inputValue = '';
+            }
+            
+
+            show();
+        }
+
+        function removeBar() {
+            numbers.splice(chosenBar - 1, 1)
+            
+            show();
+        }
+
+        function addBar(){ 
+            if (inputValue < 1 || inputValue > 10){
+            alert("Please choose a number between 1 and 10!")
+        }
+            else{
+                numbers.push(inputValue);
+                chosenBar = '<i>none</i>';
+                inputValue = '';
+            }
+
+            show();
+        }
